3c222019dd9061deee33cf7be44d936de147a668
Henrique Sales <henriquesalesmg@gmail.com>
2021-04-05 14:54:01 -0300

    Criação de plano Pagseguro Projeto

--- app/Http/Livewire/Plan/PlanCreate.php
+++ app/Http/Livewire/Plan/PlanCreate.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Plan;
 use Livewire\Component;
 use App\Models\Plan;
 use Illuminate\Support\Facades\Http;
+use App\Services\PagSeguro\Plan\PlanCreateService;
 
 class PlanCreate extends Component
 {
@@ -14,38 +15,22 @@ class PlanCreate extends Component
         'plan.name' => 'required',
         'plan.description' => 'required',
         'plan.price' => 'required',
-        'plan.slug' => 'required',
-        'plan.reference' => 'required'
+        'plan.slug' => 'required'
     ];
 
     public function createPlan() {
 
-        /*
+
         $this->validate();
 
         $plan = $this->plan;
-        $plan['reference'] = 'PAGSEGURO-REFERENCE';
+
+        $planPagSeguroReference = (new PlanCreateService())->makeRequest($plan);
+
+        $plan['reference'] = $planPagSeguroReference;
 
         Plan::create($plan);
-        */
 
-        $response = Http::withHeaders([
-            'Accept' => 'application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1',
-            'Content-Type' => 'application/json'
-        ])->post(
-            'https://ws.sandbox.pagseguro.uol.com.br/pre-approvals/request/?email=email&token=token',
-            [
-                'reference' => 'referencia-livewire',
-                'preApproval' => [
-                    'name' => 'Plano Mensal',
-                    'charge' => 'AUTO',
-                    'period' => 'MONTHLY',
-                    'amountPerPayment' => '39.99',
-                ]
-            ]
-        );
-
-        dd($response->json());
 
         session()->flash('message', 'Plano Criado com Sucesso');
     }
